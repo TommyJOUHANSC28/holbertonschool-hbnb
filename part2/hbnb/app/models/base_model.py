e1581382493248385ba3ce451c860905f2aab91d
Tommy JOUHANS <12119@holbertonstudents.com>
2026-02-18 14:31:30 +0100

    Ajouter les attributs created_at et update_at

--- part2/hbnb/app/models/base_model.py
+++ part2/hbnb/app/models/base_model.py
@@ -1,4 +1,5 @@
 import uuid
+import datetime
 
 class BaseModel:
     """
@@ -6,3 +7,5 @@ class BaseModel:
     """
     def __init__(self):
         self.id = str(uuid.uuid4())
+        self.created_at = datetime
+        self.updated_at = datetime
