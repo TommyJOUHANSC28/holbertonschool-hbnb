9927b71749d6a7974a604bbae12264a45dbafab8
Tommy JOUHANS <12119@holbertonstudents.com>
2026-02-18 14:44:56 +0100

    ajout des modules de l'entité users

--- part2/hbnb/app/models/base_model.py
+++ part2/hbnb/app/models/base_model.py
@@ -1,5 +1,5 @@
 import uuid
-import datetime
+from datetime import datetime
 
 class BaseModel:
     """
@@ -7,5 +7,11 @@ class BaseModel:
     """
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime
-        self.updated_at = datetime
+        self.created_at = datetime.now
+        self.updated_at = datetime.now
+
+    def save(self):
+        self.updated_at = self.datetime.now()
+
+    def to_dict(self):
+        return self.__dict__
--- part2/hbnb/app/models/users.py
+++ part2/hbnb/app/models/users.py
@@ -7,11 +7,20 @@ from part2.hbnb.app.models.base_model import BaseModel
 class User(BaseModel):
     def __init__(self, id, email, password, first_name, last_name, is_admin):
         super().__init__()
-        self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.password = password
         self.is_admin = True if is_admin is True else False
 
+    def register(self):
+        print(f"User {self.email} registered")
 
+    def update(self, **kwargs):
+        print(f"User {self.email} updated")
+
+    def delete(self):
+        print(f"User {self.email} deleted")
+
+    def validate(self):
+        return "@" in self.email
