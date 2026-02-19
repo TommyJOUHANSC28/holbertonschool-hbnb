42443aeabcabad12215b6c337359f063469c4d15
Tommy JOUHANS <12119@holbertonstudents.com>
2026-02-19 17:09:43 +0100

    modification du __init__

--- part2/hbnb/app/api/__init__.py
+++ part2/hbnb/app/api/__init__.py
@@ -1,8 +1,8 @@
 from flask_restx import Namespace
-from app.api import ns as users_ns
-from .places import ns as places_ns
-from .reviews import ns as reviews_ns
-from .amenities import ns as amenities_ns
+from app.api.v1.users import ns as users_ns
+from app.api.v1.places import ns as places_ns
+from app.api.v1.reviews import ns as reviews_ns
+from app.api.v1.amenities import ns as amenities_ns
 
 def register_namespaces(api):
     api.add_namespace(users_ns, path="/users")
