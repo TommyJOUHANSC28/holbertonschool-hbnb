1e7a831792ad861b6e17f3771a312dca3c81f422
James0812 <james.roussel008@gmail.com>
2026-02-19 18:29:20 +0100

    change code

--- part2/hbnb/app/api/__init__.py
+++ part2/hbnb/app/api/__init__.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
 from flask_restx import Namespace
-from .users import api as users_ns
+from .v1.users import api as users_ns
 from .places import api as places_ns
 from .reviews import api as reviews_ns
 from .amenities import api as amenities_ns
