b2a1313ba7346fadbcd8f1399b77e06d5c1ad977
Tommy JOUHANS <12119@holbertonstudents.com>
2026-02-24 17:03:25 +0100

    Retour HBnb APi is running

--- part2/hbnb/run.py
+++ part2/hbnb/run.py
@@ -6,5 +6,9 @@ from hbnb.app import create_app
 
 app = create_app()
 
+@app.route('/')
+def home():
+    return {"message": "HBnB API is running"}, 200
+
 if __name__ == '__main__':
     app.run(debug=True)
