939d2e54919f979848e838042a5f664f1fa1f5cc
James0812 <james.roussel008@gmail.com>
2026-03-04 09:37:15 +0100

    add command from flask_bcrypt import Bcrypt

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -5,12 +5,15 @@ Initializes Flask app and registers all namespaces.
 
 from flask import Flask
 from flask_restx import Api
+from flask_bcrypt import Bcrypt
 
 from hbnb.app.api.v1.users import api as users_ns
 from hbnb.app.api.v1.places import api as places_ns
 from hbnb.app.api.v1.amenities import api as amenities_ns
 from hbnb.app.api.v1.reviews import api as reviews_ns
 
+
+bcrypt = Bcrypt()
 
 def create_app():
     """
