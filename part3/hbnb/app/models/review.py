0dd1b2869d38a44f3b8158c7c485e50328d10a35
James0812 <james.roussel008@gmail.com>
2026-03-06 09:49:23 +0100

    add the code

--- part3/hbnb/app/models/review.py
+++ part3/hbnb/app/models/review.py
@@ -4,9 +4,16 @@ Supports both object references and id references.
 """
 
 from hbnb.app.models.base_model import BaseModel
+from hbnb.app import db
 
 
-class Review(BaseModel):
+class Review(BaseModel, db.Model):
+    """Review model mapped with SQLAlchemy"""
+    __tablename__ = "reviews"
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1024), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
 
     def __init__(self, text, rating,
                  user=None, place=None,
@@ -23,14 +30,14 @@ class Review(BaseModel):
         self.text = text
         self.rating = rating
 
-        # 🔥 Support tests (objects)
+        # Support tests (objects)
         if user and place:
             self.user = user
             self.place = place
             self.user_id = user.id
             self.place_id = place.id
 
-        # 🔥 Support API (ids only)
+        # Support API (ids only)
         elif user_id and place_id:
             self.user = None
             self.place = None
